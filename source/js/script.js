a72c073d4d194c49aa66e67e4d3cbc52603fe09d
constoner <constantin.kalinin@gmail.com>
2022-08-20 20:01:02 +0300

    сделал меню работающим без скрипта

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,14 @@
-const menuButton = document.querySelector(".site-navigation__button");
-const menuContainer = document.querySelector(".site-navigation__list");
+const menuButton = document.querySelector(".header__button");
+const menuContainer = document.querySelector(".site-navigation");
+const header = document.querySelector(".header--nojs")
+
+// скрытие меню по-умолчанию
+menuContainer.classList.remove("site-navigation--nojs")
+header.classList.remove("header--nojs")
 
 // переключатель состояния меню
 function menuToggle() {
-  menuContainer.classList.toggle("site-navigation__list--opened");
+  menuContainer.classList.toggle("site-navigation--opened");
 }
 
 // открытие/закрытие меню по кнопке
