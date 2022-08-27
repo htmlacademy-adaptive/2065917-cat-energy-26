e1ec5d75b26ffd65d8d00eeeb6f2a18a18983ea4
constoner <constantin.kalinin@gmail.com>
2022-08-28 01:05:39 +0300

    сделал кнопку меню и анимации меню и кнопки

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector(".header__button");
 const menuContainer = document.querySelector(".site-navigation");
 const header = document.querySelector(".header--nojs")
+const burgerButton = document.querySelector(".menu-button");
 
 // скрытие меню по-умолчанию
 menuContainer.classList.remove("site-navigation--nojs")
@@ -13,3 +14,10 @@ function menuToggle() {
 
 // открытие/закрытие меню по кнопке
 menuButton.addEventListener("click", menuToggle);
+
+// переключатель кнопки меню
+function buttonToggle() {
+  burgerButton.classList.toggle("menu-button--open");
+}
+
+burgerButton.addEventListener("click", buttonToggle);
